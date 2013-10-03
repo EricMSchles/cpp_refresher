6d6dc461716343f09b459ad867a433a2917b7531
EricSchles <ericschles@gmail.com>
2013-10-03 09:27:24 -0400

    hello world

---
+++ hello.cpp
@@ -0,0 +1,16 @@
+/*
+This is my first program in learncpp.com tutorials
+It prints hello world to the screen and then exits.
+Copied by
+Eric Schles
+*/
+
+#include <iostream> //used to import input/output functions
+
+int main()
+{
+  using namespace std;//declares the namespace, in this case std
+  cout << "Hello wold!" << endl; //prints hello world and then a newline
+  return 0; //exits the program with "successful" status
+}
+//Did it run? Yes.
